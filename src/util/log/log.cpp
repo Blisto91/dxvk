aa91b4444dc8e55ddef45cee38f62f9c2bdc9937
Blisto91 <47954800+Blisto91@users.noreply.github.com>
2025-03-26 20:18:17 +0100

    Update log.cpp

--- src/util/log/log.cpp
+++ src/util/log/log.cpp
@@ -72,7 +72,7 @@ namespace dxvk {
 
       while (std::getline(stream, line, '\n')) {
         std::stringstream outstream;
-        outstream << prefix;
+
 
         std::string adjusted = outstream.str();
 
